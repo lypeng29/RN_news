836f3bf0784977630eaee200633fa6036c3085bc
lypeng <893371810@qq.com>
2019-04-29 17:27:02 +0800

    升级reactnative,新增加载更多(没调试完)

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, Image, StyleSheet, TouchableOpacity,StatusBar } from 'react-native';
+import { Text, View, ScrollView, FlatList, Image, StyleSheet, ActivityIndicator, TouchableOpacity,StatusBar } from 'react-native';
 import SearchBar from './components/searchbar';
 import Util from '../common/util';
 import Urls from '../common/urls';
@@ -7,21 +7,27 @@ import Urls from '../common/urls';
 // import ScrollableTabView, { DefaultTabBar, ScrollableTabBar } from 'react-native-scrollable-tab-view';
 
 import TabBar from './components/tabbar';
+// import MyLoad from "../components/refreshLoad";
 
 export default class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: [],
             show: true,
             keyword: '',
-            tablist: []
+            tablist: [],
+            refresh: false,
+            listData: [],
+            animating: true,
+            loadMore: 1,
+            page: 1,
+            isload: 0 //避免多次加载
         };
     }
     componentDidMount() {
         // 初次请求数据
+        // this.onEndReachedCalled = false;
         this.getCategory();
-        // 
     }
     updateSearch = search => {
         this.setState({ keyword: search });
@@ -29,11 +35,6 @@ export default class Home extends Component {
     searchText = () => {
         this.getData();
     }
-    // 以下写法报错，不识别this
-    // searchText (){
-    //     this.getData();
-    // }
-
     getCategory(){
         var url = Urls.category_list;
         var that = this;
@@ -51,37 +52,73 @@ export default class Home extends Component {
             alert(err);
         })
     }
-    getData(cid='') {
-        // 显示loading
-        this.setState({
-            show: false
-        });
-        // 请求数据
-        var that = this;
-        var cateid = this.props.navigation.getParam('cid',cid);
-        var url = Urls.article_list + '?keyword=' + this.state.keyword + '&cid=' + cateid;
-        Util.getRequest(url, function (response) {
-            // 请求成功
-            if (!response.data || response.data.length == 0) {
-                return alert("未查询到数据");
+    getData() {
+        if (this.state.isload == 0) {
+            this.setState({
+                isload: 1
+            })
+            let newData = [];
+            if (this.state.loadMore == 1) {
+                // 发起请求
+                var that = this;
+                var cateid = this.props.navigation.getParam('cid', '');
+                var url = Urls.article_list + '?page=' + this.state.page + '&cid=' + cateid;
+                // alert(url);
+                Util.getRequest(url, function (res) {
+                    res.data.map((item, i) => {
+                        newData.push(item);
+                    });
+                    that.setState({
+                        listData: [...that.state.listData, ...newData],
+                        loadMore: res.pagination.more,
+                        page: that.state.page + 1,
+                        isload: 0
+                    })
+                }, function (res) {
+                    alert(res);
+                })
             }
-            // 显示loading,将请求结果赋值给data
-            that.setState({
-                show: true,
-                data: response.data
-            });
-        }, function (error) {
-            // 请求失败
-            alert(error);
-        });
+        }
     }
-    render() {
+    ListFooterComponent = () => {
+        return (
+            <View style={styles.bottomfoot}>
+                {
+                    this.state.listData.length != 0 ?
+                        this.state.loadMore == 0 ? (
+                            <Text style={styles.footText}>- 我是有底线的 -</Text>
+                        ) : (
+                                <View style={styles.activeLoad}>
+                                    <ActivityIndicator size="small" animating={true} />
+                                    <Text style={[styles.footText, styles.ml]}>加载更多...</Text>
+                                </View>
+                            )
+                        :
+                        null
+                }
+
+            </View>
+        );
+    }
+    _renderItem(item) {
         let full_width = Util.windowSize.width;
         let image_width = full_width - 20;
         return (
+            <TouchableOpacity style={styles.list} key={item.id} onPress={() => this.props.navigation.push('Details', { 'id': item.id })}
+                activeOpacity={0.5}>
+                <Image source={{ uri: item.thumb }} style={{ width: image_width, height: 160, marginBottom: 10 }} />
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.intro} numberOfLines={4}>{item.intro}</Text>
+                <Text style={styles.author}>by {item.author} {item.addtime}</Text>
+            </TouchableOpacity>            
+            // <Text style={styles.item}>{item.title}</Text>
+        )
+    }    
+    render() {
+        return (
             <ScrollView>
             <StatusBar
-                    animated={true} //指定状态栏的变化是否应以动画形式呈现。目前支持这几种样式：backgroundColor, barStyle和hidden
+                    animated={true} //指定状态栏的变化是否应以动画形式呈现。目前支持这几种样式：backgroundColor, barStyle和hidden
                     hidden={false}  //是否隐藏状态栏。
                     networkActivityIndicatorVisible ={false}//仅作用于ios。是否显示正在使用网络。
                     showHideTransition={'fade'}//仅作用于ios。显示或隐藏状态栏时所使用的动画效果（’fade’, ‘slide’）。
@@ -96,10 +133,31 @@ export default class Home extends Component {
                     onSubmitEditing={this.searchText}
                     onPress={this.searchText}
                 />
+                {/* <TabBar style={{marginTop:10}} ref={e => this.tabs = e} index={this.state.index} data={this.state.tablist} */}
+                    {/* onChange={(index) => { id = this.state.tablist[index].id;this.getData(id); }} /> */}
                 <TabBar style={{marginTop:10}} ref={e => this.tabs = e} index={this.state.index} data={this.state.tablist}
-                    onChange={(index) => { id = this.state.tablist[index].id;this.getData(id); }} />
+                    onChange={(index) => { id = this.state.tablist[index].id; this.props.navigation.push('Article', { 'cid': id }); }} />
 
-                {
+                <FlatList
+                    data={this.state.listData}
+                    renderItem={({ item }) => this._renderItem(item)}
+
+                    // 上拉加载更多数据
+                    onEndReachedThreshold={.2}
+                    onEndReached={() => {
+                        this.getData()
+                    }}
+
+                    ListFooterComponent={this.ListFooterComponent.bind(this)}
+
+                    // key值
+                    keyExtractor={(item, index) => item.id}
+
+                    //设置下拉加载更多的指示器的位置
+                    // progressViewOffset={50}
+
+                />
+                {/* {
                     // 请求数据时显示loading，请求成功显示列表
                     this.state.show ?
                         <View style={styles.container} >
@@ -118,7 +176,7 @@ export default class Home extends Component {
                             }
                         </View>
                         : Util.loading
-                }
+                } */}
             </ScrollView>
         )
     }
@@ -137,7 +195,9 @@ var styles = StyleSheet.create({
         borderBottomWidth: 1,
         paddingTop: 0,
         paddingBottom: 15,
-        marginBottom:15
+        marginBottom:15,
+        marginLeft:10,
+        marginRight:10
     },
     title: {
         fontWeight: "500",
@@ -155,5 +215,49 @@ var styles = StyleSheet.create({
     author:{
         fontSize:12,
         color:"#ccc"
-    }
+    },
+    item: {
+        backgroundColor: "#e8e8e8",
+        marginBottom: 10,
+        height: 150,
+        lineHeight: 150,
+        textAlign: "center",
+        color: '#000',
+        fontSize: 16
+    },
+    baseLine: {
+        width: Util.windowSize.width,
+        height: 1,
+        backgroundColor: '#eeeeee',
+    },
+    noListView: {
+        width: Util.windowSize.width,
+        height: Util.windowSize.height - 140,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    NoListText: {
+        marginTop: 15,
+        fontSize: 18,
+        color: '#999999',
+    },
+    bottomfoot: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    },
+    footText: {
+        marginTop: 5,
+        fontSize: 12,
+        color: '#999999',
+    },
+    activeLoad: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    ml: {
+        marginLeft: 10,
+    },     
 })
---
+++ src/pages/test/refresh_load2.js
@@ -0,0 +1,195 @@
+import React from 'react'
+import { Dimensions, FlatList, View, Text, StyleSheet, ActivityIndicator } from 'react-native'
+
+// get请求
+var util = {
+    getRequest: function (url, successcallback, failcallback) {
+        fetch(url)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    alert('服务器繁忙，请稍后再试；\r\nCode:' + response.status)
+                }
+            })
+            .then((responseData) => successcallback(responseData))
+            .catch((error) => failcallback(error))
+    }
+}
+// 屏幕尺寸
+var screenInfo = {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height
+};
+export default class FlatListPage extends React.Component {
+    constructor(props) {
+        super(props)        
+        this.state = {
+            refresh: false,
+            listData: [],
+            animating: true,
+            nomore: 1,
+            page:1
+        }
+    }
+    getNewData() {
+        this.setState({
+            refresh: true
+        })
+        let newData = []
+        for (let i = 0; i < 2; i++) {
+            newData.push({'title':'new data'})
+        }
+        setTimeout(() => {
+            this.setState({
+                refresh: false,
+                listData: [...newData, ...this.state.listData]
+            })
+        }, 1500)
+    }
+    componentDidMount() {
+        // this.onEndReachedCalled = false;
+        this.getData()
+    }
+    getData() {
+        let newData = [];
+        if(this.state.nomore == 1){
+            // 发起请求
+            var that = this;
+
+            util.getRequest('https://www.dpshop.net/article/index?cid=12&page=' + this.state.page,function(res){
+                res.data.map((item, i) => {
+                    newData.push(item);
+                });
+                that.setState({
+                    listData: [...that.state.listData, ...newData],
+                    nomore: res.pagination.more,
+                    page: that.state.page +1
+                }) 
+            },function(res){
+                alert(res);
+            })
+        }
+    }
+    // ListEmptyComponent(){
+    //     if(this.state.empty == false){
+    //         return (
+    //             <View style={styles.noListView}><Text style={styles.NoListText}>内容加载中...</Text></View >
+    //         )
+    //     }else{
+    //         return (
+    //             <View style={styles.noListView}><Text style={styles.NoListText}>没有数据~</Text></View>
+    //         )
+    //     }
+    // } 
+    ListFooterComponent = () => {
+        return (
+            <View style={styles.bottomfoot}>
+                {
+                    this.state.listData.length != 0 ?
+                        this.state.nomore==0 ? (
+                            <Text style={styles.footText}>- 我是有底线的 -</Text>
+                        ) : (
+                                <View style={styles.activeLoad}>
+                                    <ActivityIndicator size="small" animating={true} />
+                                    <Text style={[styles.footText, styles.ml]}>加载更多...</Text>
+                                </View>
+                            )
+                        :
+                        null
+                }
+
+            </View>
+        );
+    }
+    _renderItem(item) {
+        return (
+            <Text style={styles.item}>{item.title}</Text>
+        )
+        
+    };      
+    render() {      
+        return (
+            <View>
+                <FlatList
+                    // 渲染的数据源 8
+                    data={this.state.listData}
+
+                    renderItem={({ item }) => this._renderItem(item)}
+                    // renderItem={({ item }) => <Text style={styles.item}>{item.title}</Text>}
+
+                    // 下拉刷新数据
+                    refreshing={this.state.refresh}
+                    onRefresh={() => {
+                        this.getNewData()
+                    }}
+
+                    // 上拉加载更多数据
+                    onEndReachedThreshold={.2}
+                    onEndReached={() => {
+                        this.getData()
+                    }}
+
+                    // ListEmptyComponent={<Text style={{ textAlign: "center", marginBottom: 10 }}>暂无数据</Text>}
+                    // ListEmptyComponent={this.ListEmptyComponent.bind(this)}
+
+                    // ListFooterComponent={<Text style={{ textAlign: "center", marginBottom: 10 }}>获取更多数据</Text>}
+                    ListFooterComponent={this.ListFooterComponent.bind(this)}
+
+                    // ItemSeparatorComponent={ItemSeparatorComponent}
+
+                    // 列表key值,必须是字符串
+                    keyExtractor={(item, index) => index.toString()}
+                    //设置下拉加载更多的指示器的位置
+                    // progressViewOffset={50} 
+                    />
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    item: {
+        backgroundColor: "#e8e8e8",
+        marginBottom: 10,
+        height: 150,
+        lineHeight: 150,
+        textAlign: "center",
+        color:'#000',
+        fontSize:16
+    },
+    baseLine: {
+        width: screenInfo.width,
+        height: 1,
+        backgroundColor: '#eeeeee',
+    },
+    noListView: {
+        width: screenInfo.width,
+        height: screenInfo.height - 140,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    NoListText: {
+        marginTop: 15,
+        fontSize: 18,
+        color: '#999999',
+    },
+    bottomfoot: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+    },
+    footText: {
+        marginTop: 5,
+        fontSize: 12,
+        color: '#999999',
+    },
+    activeLoad: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    ml: {
+        marginLeft: 10,
+    },    
+})
